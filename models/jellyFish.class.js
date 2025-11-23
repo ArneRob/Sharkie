d2b4f2669201a546fa33c8df10849e3c574ceeaa
Arne Klimmt <arne-klimmt@gmx.de>
2025-11-23 10:24:37 +0100

    enemies: jellyfish is animated

--- models/jellyFish.class.js
+++ models/jellyFish.class.js
@@ -1,11 +1,32 @@
 class jellyFish extends MovableObject {
+
+    IMAGES_SWIMMING_Jelly = [
+        '../img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png',
+        '../img/2.Enemy/2 Jelly fish/Regular damage/Lila 2.png',
+        '../img/2.Enemy/2 Jelly fish/Regular damage/Lila 3.png',
+        '../img/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png',
+    ];
+
+    currentImage = 0;
+
     constructor() {
         super().loadImage('../img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png')
-
+        this.loadImages(this.IMAGES_SWIMMING_Jelly);
         this.x = 400 + Math.random() * 400
         this.y = Math.random() * 400
 
         this.width = 80
         this.height = 80
+
+        this.animate()
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_SWIMMING_Jelly.length
+            let path = this.IMAGES_SWIMMING_Jelly[i];
+            this.img = this.imageCache[path]
+            this.currentImage++;
+        }, 1000 / 5);
     }
 }

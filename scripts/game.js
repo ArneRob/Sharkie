8d5c59609a88ad3f6bf8c763bfc1fa264c6fc68f
Arne Klimmt <arne-klimmt@gmx.de>
2026-01-30 17:19:10 +0100

    git pull test

--- scripts/game.js
+++ scripts/game.js
@@ -157,3 +157,7 @@ function exitFullscreen() {
     let fullScreenIcon = document.getElementById('fullScreenIcon')
     fullScreenIcon.addEventListener("click", exitFullscreen)
 }
+
+function gitPulltest() {
+
+}

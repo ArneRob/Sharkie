34b6add24e1e717f4d507d132ebccc60b5e0090a
ArneRob <arne-klimmt@gmx.de>
2026-02-20 08:52:43 +0100

    lila jellyFish normal updated: dead animation is playing if dead then also away from dispaly

--- models/jellyFish.class.js
+++ models/jellyFish.class.js
@@ -6,6 +6,12 @@ class JellyFish extends MovableObject {
         '../img/2.Enemy/2 Jelly fish/Regular damage/Lila 3.png',
         '../img/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png',
     ];
+    IMAGES_DEAD = [
+        '../img/2.Enemy/2 Jelly fish/Dead/Lila/L1.png',
+        '../img/2.Enemy/2 Jelly fish/Dead/Lila/L2.png',
+        '../img/2.Enemy/2 Jelly fish/Dead/Lila/L3.png',
+        '../img/2.Enemy/2 Jelly fish/Dead/Lila/L4.png',
+    ];
     offset = {
         top: 12,
         left: 15,
@@ -17,6 +23,7 @@ class JellyFish extends MovableObject {
     constructor() {
         super().loadImage('../img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png')
         this.loadImages(this.IMAGES_SWIMMING);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 400
         this.y = Math.random() * 400
         this.speed = 0.15 + Math.random() * 0.25
@@ -28,9 +35,27 @@ class JellyFish extends MovableObject {
 
     animate() {
         this.moveLeft()
+        let intervalIndex = 0;
         let interval = setInterval(() => {
-            this.playAnimation(this.IMAGES_SWIMMING)
+            if (this.isColliding(world.character) && world.keyboard.SPACE) {
+                this.playDeadAnimation = true
+                intervalIndex = 0;
+                this.currentImage = 0;
+            }
+            if (this.isColliding(world.character) && !this.playDeadAnimation && world.endboss[0].endbossEnergy > 0) {
+                world.character.hurtSharkie()
+            }
+            if (this.playDeadAnimation && intervalIndex <= 12) {
+                this.playAnimation(this.IMAGES_DEAD)
+                if (intervalIndex >= 12) {
+                    this.jellyFishFlashingIsDead = true;
+                    this.x = 4000
+                }
+            } else if (!this.jellyFishFlashingIsDead) {
+                this.playAnimation(this.IMAGES_SWIMMING)
+            }
             this.pushIntervalids(interval, "jellyFishAnimationInterval", world)
-        }, 1000 / 5);
+            intervalIndex++
+        }, 1000 / 8);
     }
 }

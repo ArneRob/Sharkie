0fc03534be4354eeca883e1d5e2309ab732a352d
Arne Klimmt <arne-klimmt@gmx.de>
2026-03-02 16:18:01 +0100

    inserted endbossImgs into imageService.js

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,47 +1,4 @@
 class Endboss extends MovableObject {
-    IMAGES_SWIMMING = [
-        "../img/2.Enemy/3 Final Enemy/2.floating/1.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/2.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/3.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/4.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/5.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/6.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/7.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/8.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/9.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/10.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/11.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/12.png",
-        "../img/2.Enemy/3 Final Enemy/2.floating/13.png",
-    ];
-
-    IMAGES_FIGHT = [
-        "../img/2.Enemy/3 Final Enemy/Attack/1.png",
-        "../img/2.Enemy/3 Final Enemy/Attack/2.png",
-        "../img/2.Enemy/3 Final Enemy/Attack/3.png",
-        "../img/2.Enemy/3 Final Enemy/Attack/4.png",
-        "../img/2.Enemy/3 Final Enemy/Attack/5.png",
-        "../img/2.Enemy/3 Final Enemy/Attack/6.png",
-    ];
-    IMAGES_INTRO_ANIMATION = [
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/1.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/2.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/3.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/4.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/5.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/6.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/7.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/8.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/9.png",
-        "../img/2.Enemy/3 Final Enemy/1.Introduce/10.png",
-    ];
-    IMAGES_DEAD = [
-        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 6.png",
-        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 7.png",
-        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 8.png",
-        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 9.png",
-        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 10.png",
-    ];
 
     IMAGES_HIDDEN_ENDBOSS = [
         "../img/2.Enemy/3 Final Enemy/1.Introduce/1.png"
@@ -74,10 +31,10 @@ class Endboss extends MovableObject {
     */
     constructor() {
         super().loadImage(this.IMAGES_HIDDEN_ENDBOSS[0])
-        this.loadImages(this.IMAGES_SWIMMING);
-        this.loadImages(this.IMAGES_FIGHT);
-        this.loadImages(this.IMAGES_INTRO_ANIMATION)
-        this.loadImages(this.IMAGES_DEAD)
+        this.loadImages(returnIMAGES_Endboss_SWIMMING());
+        this.loadImages(returnIMAGES_Endboss_FIGHT());
+        this.loadImages(returnIMAGES_Endboss_INTRO())
+        this.loadImages(returnIMAGES_Endboss_DEAD())
 
         this.x = 1650
         this.y = 0
@@ -134,7 +91,7 @@ class Endboss extends MovableObject {
      * Executes the swimming animation and follows the character.
      */
     executeEndbossSwim() {
-        this.playAnimation(this.IMAGES_SWIMMING)
+        this.playAnimation(returnIMAGES_Endboss_SWIMMING())
         this.followCharacter()
     }
 
@@ -151,7 +108,7 @@ class Endboss extends MovableObject {
      */
     executeFightAnimation() {
         this.currentImage = this.endbossFightImageCounter
-        this.playAnimation(this.IMAGES_FIGHT)
+        this.playAnimation(returnIMAGES_Endboss_FIGHT())
         this.endbossFightSound.play()
         this.endbossFightImageCounter++
         this.followCharacter()
@@ -173,7 +130,7 @@ class Endboss extends MovableObject {
      * Plays the endboss intro animation.
      */
     executeEndbossIntro() {
-        this.playAnimation(this.IMAGES_INTRO_ANIMATION)
+        this.playAnimation(returnIMAGES_Endboss_INTRO())
         if (this.intervalIndex == 10) {
             this.endbossIntro = false;
         }
@@ -199,7 +156,7 @@ class Endboss extends MovableObject {
      * Executes the endboss death animation.
      */
     executeEndbossDeath() {
-        this.playAnimation(this.IMAGES_DEAD)
+        this.playAnimation(returnIMAGES_Endboss_DEAD())
         if (this.intervalIndex >= 3) {
             this.endbossDeadAnimation = false;
             this.endbossDeadAnimationIsOver = true;
--- scripts/imageService.js
+++ scripts/imageService.js
@@ -106,3 +106,57 @@ function returnIMAGES_SWIM() {
         "../img/1.Sharkie/3.Swim/5.png",
     ];
 }
+
+function returnIMAGES_Endboss_SWIMMING() {
+    return [
+        "../img/2.Enemy/3 Final Enemy/2.floating/1.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/2.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/3.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/4.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/5.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/6.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/7.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/8.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/9.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/10.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/11.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/12.png",
+        "../img/2.Enemy/3 Final Enemy/2.floating/13.png",
+    ];
+}
+
+function returnIMAGES_Endboss_FIGHT() {
+    return [
+        "../img/2.Enemy/3 Final Enemy/Attack/1.png",
+        "../img/2.Enemy/3 Final Enemy/Attack/2.png",
+        "../img/2.Enemy/3 Final Enemy/Attack/3.png",
+        "../img/2.Enemy/3 Final Enemy/Attack/4.png",
+        "../img/2.Enemy/3 Final Enemy/Attack/5.png",
+        "../img/2.Enemy/3 Final Enemy/Attack/6.png",
+    ];
+}
+
+function returnIMAGES_Endboss_INTRO() {
+    return [
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/1.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/2.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/3.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/4.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/5.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/6.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/7.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/8.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/9.png",
+        "../img/2.Enemy/3 Final Enemy/1.Introduce/10.png",
+    ];
+}
+
+function returnIMAGES_Endboss_DEAD() {
+    return [
+        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 6.png",
+        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 7.png",
+        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 8.png",
+        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 9.png",
+        "../img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 10.png",
+    ];
+}
